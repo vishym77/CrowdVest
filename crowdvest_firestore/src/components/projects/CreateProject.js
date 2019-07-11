da5c52ba47c06086eaed7a8078e413f72cfa32eb
Sheryl R <sratnam@ucsc.edu>
2019-07-11 12:25:56 -0700

    Installed and Used Thunk - T14

--- crowdvest_firestore/src/components/projects/CreateProject.js
+++ crowdvest_firestore/src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions'
 
 class CreateProject extends Component {
   state = {
@@ -46,4 +48,9 @@ class CreateProject extends Component {
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+export default connect()(CreateProject)
--- crowdvest_firestore/src/store/reducers/projectReducer.js
+++ crowdvest_firestore/src/store/reducers/projectReducer.js
@@ -9,6 +9,10 @@ const initState = {
 // in tutorial, called projectReducer
 
 const projectReducer = (state = initState, action) => {
+    switch (action.type) {
+        case 'CREATE_PROJECT':
+            console.log('created project', action.project)
+    }
     return state
 }
 
